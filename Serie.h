dfc300cc4144c1805939500f598b68a93403b13c
Miguel-Jahen <a01425106@tec.mx>
2026-06-13 20:10:59 -0600

    Update Serie.h

--- Serie.h
+++ Serie.h
@@ -1,9 +1,23 @@
 #ifndef SERIE_H
 #define SERIE_H
-#include <iostream>
+
+#include "Contenido.h"
+#include "Temporada.h"
+#include <vector>
+#include <string>
 using namespace std;
 
-class Serie{
+class Serie : public Contenido {
+private:
+    string director;
+    vector<Temporada> temporadas;
 
+public:
+    Serie();
+    Serie(string _titulo, string _genero, int _anio, string _clasificacion, double _duracion, string _director);
+    void agregarTemporada(Temporada t);
+    void mostrarInfo();
+    double calcularDuracion();
 };
+
 #endif
