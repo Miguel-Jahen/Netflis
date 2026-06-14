5261ea2bcbffba7e88c102d5beb7f8daf400a7d8
Miguel-Jahen <a01425106@tec.mx>
2026-06-14 13:06:55 -0600

    Update Serie.h

--- Serie.h
+++ Serie.h
@@ -1,9 +1,36 @@
 #ifndef SERIE_H
 #define SERIE_H
+
 #include <iostream>
+#include <vector>
+#include <string>
+#include "Contenido.h"
+#include "Temporada.h"
+#include "IListable.h"
+#include "ILikeable.h"
+
 using namespace std;
 
-class Serie{
+class Serie : public Contenido, public IListable, public ILikeable {
+private:
+    string director;
+    vector<Temporada> temporadas;
 
+public:
+    Serie();
+    Serie(int _id, string _titulo, string _genero, int _anio, string _clasificacion, double _duracion, string _director);
+
+    void mostrarInfo();
+    double calcularDuracion();
+
+    void agregarTemporada(Temporada t);
+    void eliminarTemporada(Temporada t);
+
+    void agregarALista();
+    void quitarDeLista();
+
+    void darLike();
+    void quitarLike();
 };
+
 #endif
