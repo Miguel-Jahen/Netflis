62d48b9ce2d2516b64347ce475fab24378d31264
Miguel-Jahen <a01425106@tec.mx>
2026-06-13 20:17:57 -0600

    Update Serie.cpp
    Todavía no esta terminado, necesito hacer contenido antes de poder acabar los métodos

--- Serie.cpp
+++ Serie.cpp
@@ -1,1 +1,21 @@
 #include "Serie.h"
+
+Serie::Serie() : Contenido() {
+    director = "";
+}
+
+Serie::Serie(string _titulo, string _genero, int _anio, string _clasificacion, double _duracion, string _director)
+    : Contenido(_titulo, _genero, _anio, _clasificacion, _duracion) {
+    
+    director = _director;
+}
+
+void Serie::agregarTemporada(Temporada t) {
+    temporadas.push_back(t);
+}
+
+
+void Serie::mostrarInfo(){}
+
+double Serie::calcularDuracion(){}
+  
